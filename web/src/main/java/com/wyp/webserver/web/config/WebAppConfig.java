8de2905cf0cfe181725e935ed7243052ec408e0e
wangyaping <yapingw@dingtalk.com>
2017-10-02 18:15:54 +0800

    日志异常页面配置

--- web/src/main/java/com/wyp/webserver/web/config/WebAppConfig.java
+++ web/src/main/java/com/wyp/webserver/web/config/WebAppConfig.java
@@ -2,7 +2,11 @@ package com.wyp.webserver.web.config;
 
 import com.wyp.common.util.Log;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -32,5 +36,18 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
         Log.info("imagePath:{}", imagePath);
         registry.addResourceHandler("/images/**").addResourceLocations(imagePath);
         super.addResourceHandlers(registry);
+    }
+
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+        return (container -> {
+            ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+
+            container.addErrorPages(error401Page, error404Page, error500Page);
+        });
     }
 }
